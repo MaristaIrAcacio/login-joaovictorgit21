f6109de998cdf4a58e4d21a34d1acb97ef92720b
João Victor <joaovictorgit21@gmail.com>
2023-07-04 12:13:14 -0300

    Atualizar

--- js/main.js
+++ js/main.js
@@ -7,5 +7,20 @@ function validar() {
     } else
     if (password.length < 8) {
         alert("Senha deve ter no minimo 8 caracteres");
+    } else {
+        window.location.href = "aula2.html";
+    }
+}
+
+
+var dadosLista = [];
+
+function salvarUser()
+{
+    let nomeUser = document.getElementById("nomeUser").value;
+
+    if (nomeUser) {
+        dadosLista.push(nomeUser);
+        console.log(dadosLista);
     }
 }
